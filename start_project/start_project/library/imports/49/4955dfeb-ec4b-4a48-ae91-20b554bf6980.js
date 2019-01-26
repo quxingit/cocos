464485350bcf19bcbdf75394d95967460ba7ec6d
QuXin <785176430@qq.com>
2019-01-26 11:35:44 +0800

    源码

---
+++ start_project/start_project/library/imports/49/4955dfeb-ec4b-4a48-ae91-20b554bf6980.js
@@ -0,0 +1,60 @@
+"use strict";
+cc._RF.push(module, '4955d/r7EtKSK6RILVUv2mA', 'beginbtn');
+// scripts/beginbtn.js
+
+"use strict";
+
+// Learn cc.Class:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] https://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // foo: {
+        //     // ATTRIBUTES:
+        //     default: null,        // The default value will be used only when the component attaching
+        //                           // to a node for the first time
+        //     type: cc.SpriteFrame, // optional, default is typeof default
+        //     serializable: true,   // optional, default is true
+        // },
+        // bar: {
+        //     get () {
+        //         return this._bar;
+        //     },
+        //     set (value) {
+        //         this._bar = value;
+        //     }
+        // },
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+
+    start: function start() {
+        this.node.on(cc.Node.EventType.MOUSE_DOWN, function (event) {
+            cc.director.loadScene("game");
+        }, this);
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+    },
+    onKeyDown: function onKeyDown(event) {
+        console.log(event.keyCode);
+        switch (event.keyCode) {
+            case 13:
+                cc.director.loadScene("game");
+                break;
+        }
+    }
+    // update (dt) {},
+
+});
+
+cc._RF.pop();
